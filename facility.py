1a7eff1ca7606892288afc759d7fe1e6725c4bfc
Xxiong <xxiong@cqu.edu.cn>
2018-12-13 13:17:04 +0800

    facility: add update module

--- facility.py
+++ facility.py
@@ -12,7 +12,9 @@
 
 # -*- coding: utf-8 -*- #
 
-import wx, os
+import wx, os, _thread, json, sys, urllib
+from urllib import request
+from subprocess import Popen
 import pswgen_gui
 import idnum_gui
 import xls2csv_gui
@@ -20,20 +22,98 @@ import csv2xlsx_gui
 import gbk2utf_gui
 import watermark_gui
 
+
+G_APP_NAME = "facility"
+G_VERSION = "0.0.1"
+G_UPDATE_URL = "http://www.xxiong.me/facility/"
+G_HOME_PATH = os.path.join(os.path.expanduser('~'), ".facility")
+G_CONFIG_PATH = os.path.join(G_HOME_PATH, "facility.json")
+G_LOG_PATH = os.path.join(G_HOME_PATH, "facility.log")
+
+
 class MainWindow(wx.App):
     def __init__(self):
-        self.HOME_PATH = os.path.join(os.path.expanduser('~'), ".facility")
-        self.APP_NAME = "minitools-gui"
-        self.VERSION = "0.0.1"
-        self.UPDATE_URL = "http://www.xxiong.me/"
+        self.HOME_PATH = G_HOME_PATH
+        self.APP_NAME = G_APP_NAME
+        self.VERSION = G_VERSION
+        self.UPDATE_URL = G_UPDATE_URL
         if not os.path.isdir(self.HOME_PATH):
             os.mkdir(self.HOME_PATH)
+        try:
+            _thread.start_new_thread(self.check_update, ())
+        except:
+            pass
         wx.App.__init__(self)
 
-    
+
+    def check_update(self):
+        apppath = os.path.abspath(sys.argv[0])
+        appname = os.path.split(apppath)[-1]
+        updateurl = os.path.join(G_UPDATE_URL, "update")
+        data = json.loads(request.urlopen(updateurl).read())
+        config_path = G_CONFIG_PATH
+        config = dict()
+        if os.path.exists(config_path):
+            with open(config_path, 'r') as f:
+                config = json.load(f)
+            config["facility"]["version"] = G_VERSION
+            config["facility"]["name"] = appname
+            config["facility"]["path"] = apppath
+        else:
+            config = {
+                "update": {
+                    "version": "0.0.0",
+                    "name": "-",
+                    "path": "-"
+                }, 
+                "facility": {
+                    "version": G_VERSION,
+                    "name": appname,
+                    "path": apppath
+                },
+                "latest_facility": {
+                    "version": G_VERSION,
+                    "name": appname,
+                    "path": apppath
+                }
+            }
+        remote_update = data["update"]["Windows"]
+        remote_latest = data["latest"]
+        ## 更新 update 程序
+        if remote_update["version"] > config["update"]["version"]:
+            _url = os.path.join(G_UPDATE_URL, remote_update["name"])
+            _localpath = os.path.join(G_HOME_PATH, remote_update["name"])
+            urllib.request.urlretrieve(_url, _localpath)
+            
+            config["update"]["version"] = remote_update["version"]
+            config["update"]["name"] = remote_update["name"]
+            config["update"]["path"] = _localpath
+            with open(config_path, 'w') as f:
+                json.dump(config, f)
+        ## 检查预更新版本是否为最新
+        if remote_latest["version"] > config["latest_facility"]["version"]:
+            _url = os.path.join(G_UPDATE_URL, remote_latest["name"])
+            _localpath = os.path.join(G_HOME_PATH, remote_latest["name"])
+            urllib.request.urlretrieve(_url, _localpath)
+            
+            config["latest_facility"]["version"] = remote_latest["version"]
+            config["latest_facility"]["name"] = remote_latest["name"]
+            config["latest_facility"]["path"] = _localpath
+            with open(config_path, 'w') as f:
+                json.dump(config, f)
+        ## 检查是否更新到最新版本
+        if config["latest_facility"]["version"] > config["facility"]["version"]:
+            self.updateButton.SetLabel("有新版本，点击更新")
+            self.updateButton.Enable(True)
+        else:
+            self.updateButton.SetLabel("当前已是最新版本")
+        self.config = config
+
+
     def OnInit(self):
         framestyle=wx.MINIMIZE_BOX|wx.SYSTEM_MENU|wx.CAPTION|wx.CLOSE_BOX|wx.CLIP_CHILDREN
-        frame = wx.Frame(parent=None, title=self.APP_NAME, size=(600,400), style=framestyle)
+        app_title = "%s-v%s" % (self.APP_NAME, self.VERSION)
+        frame = wx.Frame(parent=None, title=app_title, size=(600,400), style=framestyle)
         frame.SetFont(wx.Font(14, wx.DEFAULT, wx.NORMAL, wx.NORMAL))
         bg = wx.Panel(frame)
 
@@ -154,10 +234,9 @@ class MainWindow(wx.App):
         return
 
         
-
-
     def OnUpdateButton(self, event):
-        pass
+        Popen(self.config["update"]["path"])
+        wx.Exit()
 
 
 def main():
